d7e4b1e410515ea54d901b57f7c996cf29aecaed
Magniquick <67406856+Magniquick@users.noreply.github.com>
2024-08-07 08:47:41 +0530

    tf

--- ml_iit/check.py
+++ ml_iit/check.py
@@ -5,6 +5,11 @@ for i in ("test.csv", "train.csv", "valid.csv"):
 		reader = csv.reader(f, delimiter=',')
 		reader.__next__()
 		for row in reader:
+			if len(row) != 2:
+				print(row)
+			if type(row[0]) != str:
+				print(row)
+
 			if row[-1] not in types:
 				types[row[-1]] = 1
 			else:
--- ml_iit/main.py
+++ ml_iit/main.py
@@ -1,7 +1,77 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import (
+    AutoTokenizer,
+    XLMRobertaForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+)
 from datasets import load_dataset
-from pathlib import Path
-tokenizer = AutoTokenizer.from_pretrained("FacebookAI/xlm-roberta-base", use_fast=True)
-model = AutoModelForMaskedLM.from_pretrained("FacebookAI/xlm-roberta-base", num_labels=4)
-load_dataset("./dataset")
+import numpy as np
+import evaluate
+import torch
+from numba import cuda as cudaa
+devicew = cudaa.get_current_device()
+
+torch.cuda.empty_cache()
+import gc
+gc.collect()
+print(torch.cuda.memory_summary(device=None, abbreviated=False))
+
+metric = evaluate.load("accuracy")
+
+model_checkpoint = "FacebookAI/xlm-roberta-base"
+model_name = model_checkpoint.split("/")[-1]
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+def model_init():
+  devicew.reset()
+  torch.cuda.empty_cache()
+  return XLMRobertaForSequenceClassification.from_pretrained(
+		model_checkpoint, num_labels=5
+  )
+dataset = load_dataset("/content/drive/MyDrive/dataset")
+#print(dataset)
+
+def encode(examples):
+    return tokenizer(examples["text"], truncation=True, padding="max_length")
+
+
+encoded_dataset = dataset.map(encode, batched=True)
+batch_size = 8
+model_name = model_checkpoint.split("/")[-1]
+
+args = TrainingArguments(
+    f"{model_name}-finetuned",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=5,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy",
+    logging_steps=64,
+    push_to_hub=True,
+)
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+
+trainer = Trainer(
+    model_init=model_init,
+    args=args,
+    train_dataset=encoded_dataset["train"],
+    eval_dataset=encoded_dataset["test"],
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+best_run = trainer.hyperparameter_search(n_trials=10, direction="maximize")
+best_run
+for n, v in best_run.hyperparameters.items():
+    setattr(trainer.args, n, v)
+trainer.train()
+trainer.evaluate()
